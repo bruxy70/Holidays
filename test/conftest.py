740f4c51785a7deb474e7253290205ef95da57b8
Vaclav <vasek.chaloupka@hotmail.com>
2022-02-18 20:08:58 +0100

    test Good Friday

--- test/conftest.py
+++ test/conftest.py
@@ -2,22 +2,29 @@
 from datetime import datetime
 from unittest.mock import patch
 
+import homeassistant.util.dt as dt_util
 import pytest
 
 TEST_PLUGINS = "pytest_homeassistant_custom_component"  # pylint: disable=invalid-name
 FUNCTION_PATH = "custom_components.holidays.calendar.now"
 
+## Fix current date to June 1st, 2020
+@pytest.fixture(autouse=True)
+def now_fixed():
+    """Return fixed date."""
+    with patch(
+        FUNCTION_PATH,
+        return_value=datetime(2020, 4, 1, 12, 0, 0, 0),
+    ):
+        yield
+
+
 # This fixture enables loading custom integrations in all tests.
 # Remove to enable selective use of this fixture
 @pytest.fixture(autouse=True)
 def auto_enable_custom_integrations(enable_custom_integrations):
+    """Enable custom integrations."""
     yield
-
-
-@pytest.fixture(name="fixed-date")
-def mock_now():
-    with patch(FUNCTION_PATH, return_value=datetime(2017, 3, 12, 0, 0, 0)):
-        yield
 
 
 # This fixture is used to prevent HomeAssistant from attempting
--- test/test_calendar.py
+++ test/test_calendar.py
@@ -5,8 +5,6 @@ from custom_components.holidays import const
 from homeassistant.core import HomeAssistant
 from pytest_homeassistant_custom_component.common import MockConfigEntry
 
-# from unittest.mock import AsyncMock
-
 
 async def test_calendar(hass: HomeAssistant):
     """Test calendar."""
@@ -21,5 +19,6 @@ async def test_calendar(hass: HomeAssistant):
     await hass.config_entries.async_setup(config_entry.entry_id)
     await hass.async_block_till_done()
     uk_holidays = hass.states.get("calendar.uk_holidays")
-    next_date = uk_holidays.attributes["next_date"].date()
-    assert next_date == date(2022, 4, 15)
+    next_date = uk_holidays.attributes["next_date"]
+    # Good Friday
+    assert next_date.date() == date(2020, 4, 10), "Goof friday is on April 10"
